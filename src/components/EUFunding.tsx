a16017695221f490528d2b4936476d8ff9cce1e2
Rhymond <raymond.kazlausk@gmail.com>
2024-11-24 12:59:48 +0700

    update eu funding page

--- src/components/EUFunding.tsx
+++ src/components/EUFunding.tsx
@@ -5,9 +5,15 @@ export default function EUFunding() {
       <div className="bg-white rounded-xl shadow-sm p-8 mb-8">
         <h1 className="text-3xl font-bold mb-8 text-center">ES Parama</h1>
         
-        <div className="mb-8">
+        <div className="mb-8 flex flex-row gap-4">
           <img 
-            src="https://i.postimg.cc/C5S8PWt9/muir5-Iz0-Qf5s-Sc7jl-KTwjng-Ozl-PY0uq-WSX7k-Wi-Z8.jpg" 
+            src="/image001.png" 
+            alt="ES paramos logotipas" 
+            className="max-w-md mx-auto mb-8"
+          />
+
+          <img 
+            src="/image002.png" 
             alt="ES paramos logotipas" 
             className="max-w-md mx-auto mb-8"
           />
@@ -20,23 +26,23 @@ export default function EUFunding() {
 
           <p className="mb-6">
             MB Laby Studio pradėjo įgyvendinti projektą „LABY sprendimo pagrįsto dirbtinio intelekto technologija sukūrimas", 
-            projekto kodas Nr. 02-018-K-0293. Projekto vertė – 89955,21 Eur, bendrovei suteikta iki 71 631,33 Eur 
-            2021‒2027 metų Europos Sąjungos fondų lėšų. Projekto įgyvendinimo terminas – nuo 2024-09-16 iki 2025-09-30.
+            projekto kodas Nr. 02-018-K-0293. Projekto vertė – 89 955,21 Eur, bendrovei suteikta iki 71 631,33 Eur 
+            Ekonomikos gaivinimo ir atsparumo didinimo plano „Naujos kartos Lietuva“ lėšos. Projekto įgyvendinimo terminas – nuo 2024-09-25 iki 2025-09-30.
           </p>
 
           <p>
-            Projekto tikslas - sukurti dirbtinio intelekto technologijomis pagrįstą IT sprendimą personalizuoto veiklų plano, 
-            skirto ikimokyklinio (2-6 metų) amžiaus vaikų užimtumui ir ugdymui, sudarymui. Projekto metu numatyta sukurti 
-            dirbtiniu intelektu paremtą skaitmeninę platformą, kuri padės mažų vaikų (2-6 metų) tėvams rasti įtraukiančias 
+            Projekto tikslas – sukurti dirbtinio intelekto technologijomis pagrįstą IT sprendimą personalizuoto veiklų plano, 
+            skirto ikimokyklinio (2–6 metų) amžiaus vaikų užimtumui ir ugdymui, sudarymui. Projekto metu numatyta sukurti 
+            dirbtiniu intelektu paremtą skaitmeninę platformą, kuri padės mažų vaikų (2–6 metų) tėvams rasti įtraukiančias 
             ir lavinančias veiklas, pritaikytas būtent jų vaikui.
           </p>
 
-            <p>2024-09-30</p>
+          <p>2025-09-30</p>
 
-            <p>
-              Daugiau informacijos: <a href="www.esinvesticijos.lt" target="_blank">www.esinvesticijos.lt</a>
-            </p>
-        </div>
+          <p>
+            Daugiau informacijos: <a href="http://www.esinvesticijos.lt" target="_blank">www.esinvesticijos.lt</a>
+          </p>
+      </div>
       </div>
     </div>
   );
